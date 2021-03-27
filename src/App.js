16f43a51a4ac6aef3f07290f6a243211e7894093
Kanica Sabharwal <kanicasabharwal94@gmail.com>
2021-03-27 08:33:42 +0000

    Implemented search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>📚 Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>📚 Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">
+          <small>
+            <a
+              href="https://github.com/Kanica94/dictionary-webpage"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by{" "}
+            <a
+              href="https://www.linkedin.com/in/kanicasabharwal"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Kanica Sabharwal
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
