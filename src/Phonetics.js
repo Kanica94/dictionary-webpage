f7af87a6d74af9be3df09ea66b6409fa6a6c381e
Kanica Sabharwal <kanicasabharwal94@gmail.com>
2021-03-28 12:09:33 +0100

    Implemented Phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  if (props.phonetics) {
+    return (
+      <div className="Phonetics">
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+          Audio
+        </a>
+        <br />
+        {props.phonetics.text}
+      </div>
+    );
+  }
+}
